3356879b86c22d6c15c0dced9028713604111050
Rst Tiwari <79466131+RstTiwari@users.noreply.github.com>
2022-01-12 10:50:04 +0530

    First pull for updating the

---
+++ index.js
@@ -0,0 +1,59 @@
+
+  
+    function newRow1() {
+    
+        var table = document.getElementById("myTable1");
+        var row = table.insertRow(3);
+        var cell1 = row.insertCell(-1);
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = "<select> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option>s </select>";
+        cell2.innerHTML = "<input value='20'>";
+        cell3.innerHTML = "<input value='30'>";
+         
+      
+       
+        }
+        function newRow2() {
+        
+        var table = document.getElementById("myTable2");
+        var row = table.insertRow(3);
+        var cell1 = row.insertCell(-1);
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = "<select> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option>s </select>";
+        cell2.innerHTML = "<input value='20'>";
+        cell3.innerHTML = "<input value='30'>";
+         
+      
+       
+        }
+        function newRow3() {
+        
+        var table = document.getElementById("myTable3");
+        var row = table.insertRow(3);
+        var cell1 = row.insertCell(-1);
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = "<select> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option>s </select>";
+        cell2.innerHTML = "<input value='20'>";
+        cell3.innerHTML = "<input value='30'>";
+         
+      
+       
+        }
+        function newRow4() {
+        
+        var table = document.getElementById("myTable4");
+        var row = table.insertRow(3);
+        var cell1 = row.insertCell(-1);
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = "<select> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option> <option >Sangeeta Prakash RauRane</option>s </select>";
+        cell2.innerHTML = "<input value='20'>";
+        cell3.innerHTML = "<input value='30'>";
+         
+      
+       
+        }
+    
